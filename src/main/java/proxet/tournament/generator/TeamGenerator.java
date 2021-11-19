d0d47f2e27a5b4159c929342b4578b5e4c4b0b3d
Taras Tarasyuk <worktarasyuk@gmail.com>
2021-11-19 22:29:11 +0200

    added constants to TeamGenerator.java

--- src/main/java/proxet/tournament/generator/TeamGenerator.java
+++ src/main/java/proxet/tournament/generator/TeamGenerator.java
@@ -10,39 +10,39 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TeamGenerator {
 
     private static final int VEHICLE_TYPE_1 = 1;
     private static final int VEHICLE_TYPE_2 = 2;
     private static final int VEHICLE_TYPE_3 = 3;
-//    private static final int
+    private static final int AMOUNT_OF_ONE_TYPE_VEHICLE_ON_ONE_TEAM = 3;
+    private static final int TEAM_QUANTITY = 2;
+    private static final int TEAM_CAPACITY = 9;
+    private static final int TOTAL_AMOUNT_OF_ONE_TYPE_VEHICLE = AMOUNT_OF_ONE_TYPE_VEHICLE_ON_ONE_TEAM * TEAM_QUANTITY;
 
-    private List<Player> playerList = new ArrayList<>();
-    private Random randomGenerator = new Random();
+    private static final String FILE_DATA_DELIMITER = "\t";
+
+    private final Random randomGenerator = new Random();
 
     public TeamGeneratorResult generateTeams(String filePath) {
-        playerList = fullFillListWithPlayers(filePath);
+        List<Player> playerList = fullFillListWithPlayers(filePath);
         List<List<Player>> listsOfConcretePlayersVehicle = getListsOfConcretePlayersVehicle(playerList);
         List<List<Player>> lists = generateTwoListsFromSample(listsOfConcretePlayersVehicle);
-
-
-
         return new TeamGeneratorResult(
                 lists.get(0), lists.get(1)
         );
     }
 
     private List<List<Player>> generateTwoListsFromSample(List<List<Player>> listsOfConcretePlayersVehicle) {
-        List<Player> firstList = new ArrayList<>(8);
-        List<Player> secondList = new ArrayList<>(8);
+        List<Player> firstList = new ArrayList<>(TEAM_CAPACITY - 1);
+        List<Player> secondList = new ArrayList<>(TEAM_CAPACITY - 1);
         List<List<Player>> lists = new ArrayList<>();
         int k = 0;
-        while (k != 3) {
+        while (k != listsOfConcretePlayersVehicle.size()) {
             int i = 0;
             Set<Integer> alreadyGotIndexes = new HashSet<>();
-            while (i != 3) {
+            while (i != AMOUNT_OF_ONE_TYPE_VEHICLE_ON_ONE_TEAM) {
                 int index = getRandomIndex(alreadyGotIndexes);
                 alreadyGotIndexes.add(index);
                 firstList.add(listsOfConcretePlayersVehicle.get(k).get(index));
@@ -59,9 +59,9 @@ public class TeamGenerator {
     }
 
     private int getRandomIndex(Set<Integer> alreadyGotIndexes) {
-        int index = randomGenerator.nextInt(6);
-        while (alreadyGotIndexes.size()!=6 && alreadyGotIndexes.contains(index)) {
-            index = randomGenerator.nextInt(6);
+        int index = randomGenerator.nextInt(TOTAL_AMOUNT_OF_ONE_TYPE_VEHICLE);
+        while (alreadyGotIndexes.size() != 6 && alreadyGotIndexes.contains(index)) {
+            index = randomGenerator.nextInt(TOTAL_AMOUNT_OF_ONE_TYPE_VEHICLE);
         }
         return index;
     }
@@ -79,9 +79,9 @@ public class TeamGenerator {
 
     private List<Player> getPlayersWithConcreteVehicleType(int vehicleType, List<Player> playerList) {
         return playerList.stream()
-                .filter(player -> player.getVehicleType()==vehicleType)
+                .filter(player -> player.getVehicleType() == vehicleType)
                 .sorted(Comparator.comparing(Player::getWaitTime).reversed())
-                .limit(6)
+                .limit(TOTAL_AMOUNT_OF_ONE_TYPE_VEHICLE)
                 .collect(Collectors.toList());
     }
 
@@ -106,7 +106,7 @@ public class TeamGenerator {
     }
 
     private Player createPlayer(String line) {
-        String[] playerData = line.split("\t");
+        String[] playerData = line.split(FILE_DATA_DELIMITER);
         String name = playerData[0].trim();
         int waitTime = Integer.parseInt(playerData[1].trim());
         int vehicleType = Integer.parseInt(playerData[2].trim());

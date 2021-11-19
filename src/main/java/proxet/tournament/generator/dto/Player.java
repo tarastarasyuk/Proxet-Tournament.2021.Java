f7297d51139d195e571524469f1d4a2401a8d3c1
Taras Tarasyuk <worktarasyuk@gmail.com>
2021-11-19 22:16:18 +0200

    added field and changed constructor

--- src/main/java/proxet/tournament/generator/dto/Player.java
+++ src/main/java/proxet/tournament/generator/dto/Player.java
@@ -4,9 +4,12 @@ public class Player {
 
     private final String nickname;
     private final int vehicleType;
+    //
+    private final int waitTime;
 
-    public Player(String nickname, int vehicleType) {
+    public Player(String nickname, int waitTime, int vehicleType) {
         this.nickname = nickname;
+        this.waitTime = waitTime;
         this.vehicleType = vehicleType;
     }
 
@@ -16,5 +19,18 @@ public class Player {
 
     public int getVehicleType() {
         return vehicleType;
+    }
+
+    public int getWaitTime() {
+        return waitTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "nickname='" + nickname + '\'' +
+                ", vehicleType=" + vehicleType +
+                ", waitTime=" + waitTime +
+                '}';
     }
 }
